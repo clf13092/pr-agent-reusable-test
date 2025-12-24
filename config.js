527e2ec5cf7a3a8730f0bce9ee46f4babf90b05d
Sugimori <66812608+clf13092@users.noreply.github.com>
2025-12-24 09:13:22 +0900

    Change AWS secret access key
    Updated AWS secret access key in config.js.

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const config={
     // AWS認証情報のハードコーディング
     aws:{
         accessKeyId:"AKIAIOSFODNN7EXAMPLE",
-        secretAccessKey:"wJalrXUtnFEMI/K7MDENG/bPxRYEXAMPLExxxx",
+        secretAccessKey:"wJalrXUtnFEMI/K7MDENG/bPYEXAMPLExxxx",
         region:"us-east-1"
     }
 };
